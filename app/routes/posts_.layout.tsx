50e5061b1a8ac0ba855a63eadd7d4877e0fccaa6
Tarun Tailor <tarun@crimecheck.in>
2023-09-07 12:39:06 +0530

    Opt In =  With Layout of Parent Component (posts.new.tsx) & Opt Out = Wihtout Layout of Parent Component (posts_.layout.tsx) added

--- app/routes/posts_.layout.tsx
+++ app/routes/posts_.layout.tsx
@@ -1,8 +1,8 @@
 export default function Newpost() {
     return (
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-        <h1>Nexted Routes = Newpost Component</h1>
-        <p>Opt In =  With Layout of Parent Component</p>
+        <h1>Nexted Route = Layout Component</h1>
+        <p>Opt Out = Wihtout Layout of Parent Component</p>
       </div>
     );
 }
